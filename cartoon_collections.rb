0732caf2b6da542a25bf6a8c55e21343101d89a4
Keith Williams <thestreetkw@hotmail.com>
2020-10-14 03:33:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,11 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.select do |element| 
-    element === "Earth!" || "Wind!" || "Fire!" || "Water!" || "Heart!"
+    element === "Earth!" || 
+    element ==="Wind!" || 
+    element ==="Fire!" || 
+    element ==="Water!" || 
+    elements === "Heart!"
     
   end 
 end
