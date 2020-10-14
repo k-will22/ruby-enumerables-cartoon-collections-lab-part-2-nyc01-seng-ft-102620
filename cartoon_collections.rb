89766cdbb5ba851b657934e06ccee7f2933b73e4
Keith Williams <thestreetkw@hotmail.com>
2020-10-14 03:21:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,4 +30,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.any? do |element|
     element == "Earth!" || "Wind!" || "Fire!" || "Water!" || "Heart!"
+  end 
 end
